81f91cc58d50f3335cb6af5b44cf89252957d971
X-AROK <vanyanadeev@mail.ru>
2023-08-07 09:27:27 +0300

    Added WeakKeyDictionary in Attribute class

--- pyperms/conditions/attribute.py
+++ pyperms/conditions/attribute.py
@@ -1,21 +1,21 @@
-from typing import Any, Dict
+from typing import Any
+from weakref import WeakKeyDictionary
 
 from pyperms.conditions.utils import getattr_recursive
 
 
 class Attribute:
     __name: str
-    __values: Dict[object, Any]
+    __values: WeakKeyDictionary
 
     def __init__(self, name: str) -> None:
         self.__name = name
-        self.__values = {}
+        self.__values = WeakKeyDictionary()
 
     def get_value(self, obj: object) -> Any:
-        id_ = id(obj)
-        if id_ not in self.__values:
-            self.__values[id_] = getattr_recursive(obj, self.__name)
-        return self.__values[id_]
+        if obj not in self.__values:
+            self.__values[obj] = getattr_recursive(obj, self.__name)
+        return self.__values[obj]
 
 
 def attr(name: str) -> Attribute:
